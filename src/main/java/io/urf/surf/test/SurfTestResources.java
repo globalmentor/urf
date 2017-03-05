b403394d292e24e5be6f05d7c3f96467fb688c16
Garret Wilson <garret@globalmentor.com>
2017-03-05 19:39:10 -0300

    Added basic string, Boolean, and various object properties test SURF files.

--- src/main/java/io/urf/surf/test/SurfTestResources.java
+++ src/main/java/io/urf/surf/test/SurfTestResources.java
@@ -26,6 +26,7 @@ import java.util.Set;
  */
 public class SurfTestResources {
 
+	//simple files
 	public static final String OK_SIMPLE_SURF_RESOURCE_NAME = "ok-simple.surf";
 	public static final String OK_SIMPLE_COMMENT_SURF_RESOURCE_NAME = "ok-simple-comment.surf";
 	public static final String OK_SIMPLE_COMMENT_WHITESPACE_SURF_RESOURCE_NAME = "ok-simple-comment-whitespace.surf";
@@ -35,6 +36,41 @@ public class SurfTestResources {
 	/** Names of SURF document resources semantically equivalent to {@value #OK_SIMPLE_SURF_RESOURCE_NAME}. */
 	public static Set<String> OK_SIMPLE_RESOURCE_NAMES = immutableSetOf(OK_SIMPLE_SURF_RESOURCE_NAME, OK_SIMPLE_NO_EOL_SURF_RESOURCE_NAME,
 			OK_SIMPLE_MULTILINE_SURF_RESOURCE_NAME, OK_SIMPLE_COMMENT_SURF_RESOURCE_NAME, OK_SIMPLE_COMMENT_WHITESPACE_SURF_RESOURCE_NAME);
+
+	//objects
+	public static final String OK_OBJECT_NO_PROPERTIES_0_RESOURCE_NAME = "ok-object-no-properties-0.surf";
+	public static final String OK_OBJECT_NO_PROPERTIES_1_RESOURCE_NAME = "ok-object-no-properties-1.surf";
+	public static final String OK_OBJECT_NO_PROPERTIES_2_RESOURCE_NAME = "ok-object-no-properties-2.surf";
+
+	/** Names of SURF document resources semantically equivalent to {@value #OK_OBJECT_NO_PROPERTIES_0_RESOURCE_NAME}. */
+	public static Set<String> OK_OBJECT_NO_PROPERTIES_RESOURCE_NAMES = immutableSetOf(OK_OBJECT_NO_PROPERTIES_0_RESOURCE_NAME,
+			OK_OBJECT_NO_PROPERTIES_1_RESOURCE_NAME, OK_OBJECT_NO_PROPERTIES_2_RESOURCE_NAME);
+
+	public static final String OK_OBJECT_ONE_PROPERTY_0_RESOURCE_NAME = "ok-object-one-property-0.surf";
+	public static final String OK_OBJECT_ONE_PROPERTY_1_RESOURCE_NAME = "ok-object-one-property-1.surf";
+	public static final String OK_OBJECT_ONE_PROPERTY_2_RESOURCE_NAME = "ok-object-one-property-2.surf";
+
+	/** Names of SURF document resources semantically equivalent to {@value #OK_OBJECT_ONE_PROPERTY_0_RESOURCE_NAME}. */
+	public static Set<String> OK_OBJECT_ONE_PROPERTY_RESOURCE_NAMES = immutableSetOf(OK_OBJECT_ONE_PROPERTY_0_RESOURCE_NAME,
+			OK_OBJECT_ONE_PROPERTY_1_RESOURCE_NAME, OK_OBJECT_ONE_PROPERTY_2_RESOURCE_NAME);
+
+	public static final String OK_OBJECT_TWO_PROPERTIES_0_RESOURCE_NAME = "ok-object-two-properties-0.surf";
+	public static final String OK_OBJECT_TWO_PROPERTIES_1_RESOURCE_NAME = "ok-object-two-properties-1.surf";
+	public static final String OK_OBJECT_TWO_PROPERTIES_1_NO_COMMAS_RESOURCE_NAME = "ok-object-two-properties-1-no-commas.surf";
+	public static final String OK_OBJECT_TWO_PROPERTIES_2_RESOURCE_NAME = "ok-object-two-properties-2.surf";
+	public static final String OK_OBJECT_TWO_PROPERTIES_2_NO_COMMAS_RESOURCE_NAME = "ok-object-two-properties-2-no-commas.surf";
+
+	/** Names of SURF document resources semantically equivalent to {@value #OK_OBJECT_TWO_PROPERTIES_0_RESOURCE_NAME}. */
+	public static Set<String> OK_OBJECT_TWO_PROPERTIES_RESOURCE_NAMES = immutableSetOf(OK_OBJECT_TWO_PROPERTIES_0_RESOURCE_NAME,
+			OK_OBJECT_TWO_PROPERTIES_1_RESOURCE_NAME, OK_OBJECT_TWO_PROPERTIES_1_NO_COMMAS_RESOURCE_NAME, OK_OBJECT_TWO_PROPERTIES_2_RESOURCE_NAME,
+			OK_OBJECT_TWO_PROPERTIES_2_NO_COMMAS_RESOURCE_NAME);
+
+	//urf-Boolean
+	public static final String OK_BOOLEAN_TRUE_RESOURCE_NAME = "ok-boolean-true.surf";
+	public static final String OK_BOOLEAN_FALSE_RESOURCE_NAME = "ok-boolean-false.surf";
+
+	//urf-String
+	public static final String OK_STRING_FOOBAR_RESOURCE_NAME = "ok-string-foobar.surf";
 
 	public static final String OK_BOOLEANS_RESOURCE_NAME = "ok-booleans.surf";
 	public static final String OK_NUMBERS_RESOURCE_NAME = "ok-numbers.surf";
